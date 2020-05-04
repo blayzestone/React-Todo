089053977725f8e91ffa57be4a5e3e937538d7f4
Blayze <blayzestone@protonmail.com>
2020-05-04 13:05:25 -0700

    Setup TodoList and Todo components

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Todo = ({ todo }) => {
+  return(
+      <p
+      style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.task}
+      </p>
+  );
+}
+
+export default Todo;
