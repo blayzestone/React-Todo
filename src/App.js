8276922be11852bd9afb661e77a9386019b1fb78
Blayze <blayzestone@protonmail.com>
2020-05-04 15:42:18 -0700

    add local storage to app

--- src/App.js
+++ src/App.js
@@ -7,20 +7,19 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ],
+      todos: [],
     }
   }
+
+  componentDidMount() {
+    this.setState({
+      todos: JSON.parse(localStorage.getItem("todos")),
+    });
+  }
+  componentDidUpdate() {
+    localStorage.setItem("todos", JSON.stringify(this.state.todos));
+  }
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   addItem = itemName => {
